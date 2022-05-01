119655346f29d993b88aa88a3f9e4ef47e64a75a
iulian Preuteasa <iulian_preuteasa@yahoo.com>
2022-05-01 19:59:21 +0100

    deleting getBookByAuthor for the moment

--- queries.js
+++ queries.js
@@ -7,15 +7,27 @@ const pool = new Pool({
   port: 5432,
 });
 
-const getBooks = (request, response) => {
+const getBooks = (req, res) => {
   pool.query("SELECT * FROM books ORDER BY id ASC", (error, results) => {
     if (error) {
       throw error;
     }
-    response.status(200).json(results.rows);
+    res.status(200).json(results.rows);
   });
 };
 
+const getBookById = (req, res) => {
+  const id = parseInt(req.params.id);
+  pool.query("SELECT * FROM books WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.json(results.rows);
+  });
+};
+
+
 module.exports = {
   getBooks,
+  getBookById,
 };
--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const app = express();
 const port = 3000;
 const db = require("./queries");
 
-// app.get("/", (req, res) => {
-//   res.send("Welcome to my eCommerce site!");
-// });
+app.get("/", (req, res) => {
+  res.send("Welcome to my eCommerce site!");
+});
+
+app.get("/books", db.getBooks);
+app.get("/books/:id", db.getBookById);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
-
-app.get("/", db.getBooks);
